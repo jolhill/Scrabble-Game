5027849ea230904dd2c11fae346802ad5a57b465
Samuel Hinkhouse <shinkhouse@users.noreply.github.com>
2016-03-07 22:36:03 -0600

    Started GUI Class

---
+++ GUIClass.java
@@ -0,0 +1,83 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+public class GUIClass extends JFrame {
+
+	private JButton squares[][];
+	private JPanel content;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUIClass frame = new GUIClass();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public GUIClass() {
+		setTitle("Scrabble\r\n");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(500, 500, 650, 500);
+		content = new JPanel();
+		content.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(content);
+		content.setLayout(new BorderLayout(0, 0));
+
+		JTextPane introduction = new JTextPane();
+		introduction.setEditable(false);
+		introduction.setText("Welcome to Scrabble!");
+		content.add(introduction, BorderLayout.NORTH);
+
+		JTextPane playerScoresAndInfo = new JTextPane();
+		playerScoresAndInfo.setEditable(false);
+		playerScoresAndInfo.setText("These are where player scores go");
+		content.add(playerScoresAndInfo, BorderLayout.EAST);
+
+		ButtonHandler bh = new ButtonHandler();
+		
+		//Nests a JPanel inside BorderLayout Center
+		JPanel panel = new JPanel();
+		content.add(panel, BorderLayout.CENTER);
+		//Creates GridLayout Inside Panel
+		panel.setLayout(new GridLayout(15, 15));
+		squares = new JButton[15][15];
+		
+		//Makes buttons
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				squares[i][j] = new JButton();
+				squares[i][j].setText("*");
+				panel.add(squares[i][j]);
+				squares[i][j].addActionListener(bh);
+
+			}
+
+		}
+
+	}
+
+	private class ButtonHandler implements ActionListener {
+		public void actionPerformed(ActionEvent ae) {
+			for (int i = 0; i < 15; i++) {
+				for (int j = 0; j < 15; j++) {
+
+					if (ae.getSource() == squares[i][j]) {
+						squares[i][j].setText("@");
+					}
+
+				}
+
+			}
+		}
+
+	}
+}
