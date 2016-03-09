f890aebd4b2db00482e2b8cae8ae73c08365cff6
Samuel Hinkhouse <shinkhouse@users.noreply.github.com>
2016-03-09 09:39:27 -0600

    Changed Layout of Game Board

--- GUIClass.java
+++ GUIClass.java
@@ -1,5 +1,6 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -38,14 +39,16 @@ public class GUIClass extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(500, 500, 650, 500);
 		content = new JPanel();
-		content.setBorder(new EmptyBorder(5, 5, 5, 5));
+		
 		setContentPane(content);
 		content.setLayout(new BorderLayout(0, 0));
 
 		JTextPane introduction = new JTextPane();
+		introduction.setPreferredSize( new Dimension( 200, 50 ) );
 		introduction.setEditable(false);
-		introduction.setText("Welcome to Scrabble!");
-		introduction.setBackground(Color.red);
+		introduction.setFont(new Font("Tahoma", Font.BOLD, 20));
+		introduction.setText("Scores will go here in this format: \"Player 1 : 0 Player 2: 0\"");
+		introduction.setBackground(Color.WHITE);
 		content.add(introduction, BorderLayout.NORTH);
 
 		bh = new ButtonHandler();
@@ -54,35 +57,41 @@ public class GUIClass extends JFrame {
 		JPanel panel = new JPanel();
 		content.add(panel, BorderLayout.CENTER);
 		//Creates GridLayout Inside Panel
+		panel.setBorder(new EmptyBorder(30, 30, 30, 30));
 		panel.setLayout(new GridLayout(15, 15));
 		
-		scores = new JTextPane();
-		content.add(scores, BorderLayout.WEST);
-		scores.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		scores.setEditable(false);
-		scores.setText("Score: 0-0");
-		scores.setBackground(Color.lightGray);
-		squares = new JButton[15][15];
-		setSize(1000,800);
+		
 		//Makes buttons
+		squares = new JButton[15][15];
 		for (int i = 0; i < 15; i++) {
 			for (int j = 0; j < 15; j++) {
 				squares[i][j] = new JButton();
 				squares[i][j].setText("*");
 				panel.add(squares[i][j]);
 				squares[i][j].addActionListener(bh);
-				squares[i][j].setBackground(Color.yellow);
+				squares[i][j].setBackground(new Color(245, 222, 179));
+				squares[i][j].setBorder(new LineBorder(new Color(244, 164, 96)));
 			}
 
 		}
-		hands = new JTextField("HANDS GO HERE");
+		hands = new JTextField("HANDS GO HERE (If we put hands here we need to clean up how we print the hands out... but scores could go here too.)");
+		
 		hands.setBackground(Color.CYAN);
 		content.add(hands, BorderLayout.SOUTH);
+		hands.setPreferredSize( new Dimension( 100, 60 ) );
+		hands.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		
+		setSize(800,800);
+		setResizable(false);
 		setVisible(true);
 	}
 	
 	public void setHands(String hand){
-		scores.setText(hand);
+		hands.setText(hand);
+	}
+	
+	public void setScores(String score){
+		hands.setText(score);
 	}
 	
 	public void playChar(char c,int x,int y){
@@ -106,7 +115,7 @@ public class GUIClass extends JFrame {
 						iSpotClicked [0] = j;
 						iSpotClicked[1] = i;
 						clicked = true;
-						wakeUp();
+						//wakeUp();
 						return;
 					}
 				}
