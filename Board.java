2d4256206052242c5187f8791ee19f0af658621a
jolhill <joshua.hill55@gmail.com>
2016-03-10 22:25:55 -0600

    Fixed Orientation Issue/Deleted Old Unused Code

--- Board.java
+++ Board.java
@@ -33,24 +33,7 @@ public class Board {
 	public char getSquare(int iRow,int iCollumn){
 		return(board[iRow][iCollumn].charAt(1));
 	}
-	public Object [] getInput(){
-		Object [] lDirections = {"Horizontal","Vertical"};
-		//keep null, Prompt, Title of box, Type of message, icon {ignore this}, object[] containing options, default anwser
-		String sOrientation = (String)JOptionPane.showInputDialog(null,
-				"Select word orientation", "Orientation",
-				JOptionPane.PLAIN_MESSAGE, null, lDirections, "Vertical");
-		Object [] lNums = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-		int iRow = (int)JOptionPane.showInputDialog(null,
-				"Select row number", "Row",
-				JOptionPane.PLAIN_MESSAGE, null, lNums, "1");
-		Object [] lChars = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'};
-		char cCollumn = (char)JOptionPane.showInputDialog(null,
-				"Select collumn", "Collumn",
-				JOptionPane.PLAIN_MESSAGE, null, lChars, "A");
-		Object [] aInput = {sOrientation,iRow,cCollumn};
-		return(aInput);
-	}
-	
+
 	//POSSIBLE ARRAY OUT OF BOUNDS EXCEPTION IF USER DOESN'T
 	//LEAVE ENOUGH ROOM FOR THE WORD
 	public boolean setWord(String sWord,ArrayList<Tile> aPlayerPile, GUIClass gui){
@@ -90,36 +73,10 @@ public class Board {
 		else{
 			for(int i = 0; i < sWord.length(); i++){
 				board[iRow][iCollumn] = "[" + sWord.charAt(i)+"]";
-				gui.playChar(sWord.charAt(i), iCollumn, iRow);
+				gui.playChar(sWord.charAt(i), iRow, iCollumn);
 				iCollumn++;
 			}
 		}
 		return true;
 	}
-	
-	//Prints out the Game Board
-	public void getBoard() {
-		String sTitle = "*";
-		System.out.printf("%-6s",sTitle);
-		for (int i  = 0; i < cAlpha.length; i++) {
-			char cAlphabet  = cAlpha[i];
-			System.out.printf("%-3C", cAlphabet);
-		}
-		
-		System.out.println();
-		String sBoard = "";
-		for (int i = 0; i < board.length; i++) {
-			String sNumbers  = numbers[i] + ": ";
-			System.out.printf("%-5s",sNumbers);
-			
-			for (int j = 0; j < board[i].length; j++) {
-				sBoard = board[i][j]; 
-				System.out.printf("%-2s",sBoard);
-			}
-			System.out.println();
-		}
-		
-	}
-
-	
 }
